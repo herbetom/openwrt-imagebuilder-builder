6f13f59ff1ed69e9c26d4e08c8b7e23b1980e15d
Tom Herbers <mail@tomherbers.de>
2023-11-07 09:52:09 +0100

    feeds: set them manually and custom

--- contrib/actions/prepare-openwrt.sh
+++ contrib/actions/prepare-openwrt.sh
@@ -15,3 +15,10 @@ cd openwrt
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+touch feeds.conf
+
+echo "src-git packages https://github.com/herbetom/openwrt-packages.git;openwrt-23.05-quectel-timesync" >> feeds.conf
+#echo "src-git packages https://git.openwrt.org/feed/packages.git;openwrt-23.05" >> feeds.conf
+echo "src-git luci https://git.openwrt.org/project/luci.git;openwrt-23.05" >> feeds.conf
+echo "src-git routing https://git.openwrt.org/feed/routing.git;openwrt-23.05" >> feeds.conf
+echo "src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-23.05" >> feeds.conf
